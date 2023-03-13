34f58d4a6c73220890395be7c86decb05453dbd2
Bobo100 <33471758+Bobo100@users.noreply.github.com>
2023-03-13 14:57:34 +0800

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,20 +6,21 @@ import { InputTextLimitLength, InputTextOnlyAlphabet, InputTextOnlyNumber, Input
 export const Home = () => {
 
     const [inputText, setInputText] = useState('');
-    const handleInput = (value: string) => {
+    const handleInputChange = (value: string) => {
         setInputText(value);
     };
 
     const [labelText, setLabelText] = useState('');
-    const handleInputChange = (value: string) => {
+    const handleSubmit = (value: string) => {
         setLabelText(value);
     };
+
 
     // 設定Enter可以觸發搜尋
     const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
         if (e.key === 'Enter') {
             e.preventDefault();
-            handleInputChange(inputText);
+            handleSubmit(inputText);
         }
     };
 
@@ -47,8 +48,8 @@ export const Home = () => {
     return (
         <div className="home">
             <label htmlFor="search">Welcom {labelText}</label>
-            <InputText value={inputText} onChange={handleInput} onKeyPress={handleKeyDown} />
-            <BtnSubmit value={inputText} onClick={handleInputChange} />
+            <InputText value={inputText} onChange={handleInputChange} onKeyPress={handleKeyDown} />
+            <BtnSubmit value={inputText} onClick={handleSubmit} />
 
             <h3>測試輸入限制</h3>
             <p>Only數字</p>
